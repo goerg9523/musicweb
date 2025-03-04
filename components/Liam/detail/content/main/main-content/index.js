f6fec25339a76499fecb808d4b0da04f8b68d904
goerg9523 <goerg9523@gmail.com>
2025-03-04 16:55:07 +0800

    delete others

--- components/Liam/detail/content/main/main-content/index.js
+++
@@ -1,3 +0,0 @@
-const { FaRegFileCode } = require("react-icons/fa");
-
-FaRegFileCode
--- components/Liam/detail/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import DetailTop from './top'
-export default function index() {
-  return (
-    <div>
-      <DetailTop />
-    </div>
-  )
-}
